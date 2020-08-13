b930b7091c7d511d57b762267f339fd5d8dc2d5b
2kays <2kays@users.noreply.github.com>
2020-08-13 23:22:18 +0100

    Generalise interpreter operations

--- Main.scala
+++ Main.scala
@@ -3,29 +3,59 @@ package scala_adventures
 import scala.collection.immutable.List
 import scala.collection.mutable.ArrayStack
 
-sealed trait Op
-case class Push(item: Int) extends Op
-case object Add extends Op
-case object Pop extends Op
-case object PeekNPrint extends Op
+class InterpreterStack extends ArrayStack[Int]
+
+trait Op {
+  def iapply(stack: InterpreterStack)
+}
+
+sealed abstract class ArithmeticBinOp(f: (Int, Int) => Int) extends Op {
+  def iapply(stack: InterpreterStack) = stack.combine(f)
+}
+
+final case object Add extends ArithmeticBinOp(_ + _)
+final case object Sub extends ArithmeticBinOp(_ - _)
+final case object Mul extends ArithmeticBinOp(_ * _)
+final case object Div extends ArithmeticBinOp(_ / _)
+final case object Mod extends ArithmeticBinOp(_ % _)
+// final case object Pow extends ArithmeticBinOp(Math.pow(_, _))
+
+final case class Push(item: Int) extends Op {
+  def iapply(stack: InterpreterStack) = stack.push(item)
+}
+
+final case object Pop extends Op {
+  def iapply(stack: InterpreterStack) = stack.pop()
+}
+
+final case object PeekNPrint extends Op {
+  def iapply(stack: InterpreterStack) = println(stack.head)
+}
+
+final case object Dup extends Op {
+  def iapply(stack: InterpreterStack) = stack.dup()
+}
 
 class Interpreter(val program: List[Op]) {
-  var stack = new ArrayStack[Int]
+  var stack = new InterpreterStack
 
   def run: Unit = run(program)
 
-  def run(fragment: List[Op]): Unit = {
+  def run(fragment: List[Op], counter: Int = 0): Unit = {
     val x::xs = fragment
 
-    (x: Op) match {
-      case Push(item) => stack.push(item)
-      case Pop => stack.pop()
-      case Add => stack.push(stack.pop() + stack.pop())
-      case PeekNPrint => println(stack.head)
+    try {
+      x.iapply(stack)
+    } catch {
+      case (e: Throwable) => {
+        val ename = e.getClass.getSimpleName
+        println(s"Error L${counter}: ${ename}")
+        return
+      }
     }
 
     xs.headOption match {
-      case Some(_) => run(xs)
+      case Some(_) => run(xs, counter + 1)
       case None => return
     }
   }
@@ -35,7 +65,9 @@ object Main extends App {
   val testProgram = List[Op](
     Push(10),
     Push(20),
+    Dup,
     Add,
+    Sub,
     PeekNPrint,
     Pop
   )
